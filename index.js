08c05a1adf4fe3b00301450a377a221dfa137e39
Me <pdprince.lfc+github@gmail.com>
2024-06-01 17:11:03 -0400

    ezpz

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+    for (let i = 0; i < array.length; i++) {
+        const first = array[i];
+        for (let j = 0; j < array.length; j++) {
+            if (j === i) {
+                continue;
+            }
+            const second = array[j];
+            if ((second + first) === target) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
@@ -16,19 +29,29 @@ function hasTargetSum(array, target) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+    console.log("Expecting: false");
+    console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+    console.log("");
+
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([1, 2, 5, 5, 2], 4));
+
+    console.log("");
+
+    console.log("Expecting: false");
+    console.log("=>", hasTargetSum([1, 2, 5, 5, 2], 1));
 }
 
 module.exports = hasTargetSum;
